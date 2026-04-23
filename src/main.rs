1d959ea9083f028e40ab201b822ba0c0c6358e1b
Marcos Ruiz <marcos.ruizmc05@gmail.com>
2026-04-22 21:05:31 -0300

    Proyecto rehecho para consolidar los conocimientos

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,57 @@
-use std::net::TcpStream;
-use std::thread;
-use std::time::Duration;
-
+use std::net::{SocketAddr, TcpStream};// Permite hacer conexiones TPC
+use std::thread; // Permite crear hilos
+use std::time::Duration; // permite manejar tiempos 
+//use std::env; // Usamos el entorno de RUST
+use std::sync::mpsc;// comunicación entre hilos
 fn main() {
+let ip = "127.0.0.1";
+let inicio = 1;
+let fin = 500;
+let inicio2 = 501;
+let fin2 = 1000;
+let timeout = Duration::from_millis(30);
 
-    let _timeout = Duration::from_millis(200);
+// Creamos un canal para comunicar los hilos con el hilo principal
+let (tx,rx) = mpsc::channel();
 
-    let hilo1 = thread::spawn(||{
-        let ip = "127.0.0.1";
-        for puerto in 1..500{
-            let ip_puerto = format!("{}:{}", ip, puerto);
-            let direccion = &ip_puerto.parse().unwrap();
-                match TcpStream::connect_timeout(&direccion, _timeout) {
-                Ok(_) => println!("Puerto {} abierto",puerto),
-                Err(_) => (),      
-            }       
-        }
+// Clonamos el transmisor para que cada hilo tenga su propia copia
+let tx1 = tx.clone();
+let tx2 = tx.clone();
 
-    });
+let hilo1 = thread::spawn(move ||{
+    for puerto in inicio..=fin{
+         let ip_puerto = format!("{}:{}",ip ,puerto);
+         let direccion: SocketAddr = ip_puerto.parse().unwrap();
+         if TcpStream::connect_timeout(&direccion, timeout).is_ok(){
+            tx1.send(format!("[OPEN]{} ",puerto)).unwrap();
 
-    let hilo2 = thread::spawn(||{
-        let ip = "127.0.0.1";
-        for puerto in 500..1000{
-            let ip_puerto = format!("{}:{}", ip, puerto);
-            let direccion = &ip_puerto.parse().unwrap();
-                match TcpStream::connect_timeout(&direccion, _timeout) {
-                Ok(_) => println!("Puerto {} abierto",puerto),
-                Err(_) => (),
-            }    
-        }
-    });    
+            
+            
+    }
+}
+});
 
-    let _ = hilo1.join().unwrap();
-    let _ = hilo2.join().unwrap();
+let hilo2 = thread::spawn(move ||{
+    for puerto in inicio2..=fin2{
+         let ip_puerto = format!("{}:{}",ip ,puerto);
+         let direccion: SocketAddr = ip_puerto.parse().unwrap();
+         if TcpStream::connect_timeout(&direccion, timeout).is_ok(){
+             tx2.send(format!("[OPEN]{} ",puerto)).unwrap();
+    }
+}
+});
+
+drop(tx); //Cerramos el canal principal
+
+for mensaje in rx{
+    println!("{}",mensaje);
+}
+
+
+
+hilo1.join().unwrap();
+hilo2.join().unwrap();
+
+
+
 }
